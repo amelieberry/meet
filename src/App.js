8ae44739f6f24a3b5b429f9cbfedd6d1ce8c1c08
Amelie <aac.berry@gmail.com>
2022-11-25 18:38:30 +0100

    add eventlist component to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import EventList from './EventList';
 
 function App() {
   return (
     <div className="App">
-      
+      <EventList />
     </div>
   );
 }
