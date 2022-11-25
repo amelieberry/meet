b606f3fd61190b320e8c96ac8f0a7cda66367275
Amelie <aac.berry@gmail.com>
2022-11-25 18:38:01 +0100

    create EventList test

---
+++ src/__tests__/EventList.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow } from "enzyme";
+import EventList from "../EventList";
+import Event from '../Event';
+
+describe('<EventList /> component', () => {
+    test('render correct number of events', () => {
+        const EventListWrapper = shallow(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
+        expect(EventListWrapper.find(Event)).toHaveLength(4);
+    });
+});
