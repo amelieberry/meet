a351d06b65310ba29771a119bdd62c4296a474bf
Amelie <aac.berry@gmail.com>
2022-11-29 12:54:49 +0100

    import CitySearch component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import EventList from './EventList';
+import CitySearch from './CitySearch';
 
 function App() {
   return (
     <div className="App">
+      <CitySearch />
       <EventList />
     </div>
   );
