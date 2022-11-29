49154178b483cae9ee151c61a8781b3bf2629378
Amelie <aac.berry@gmail.com>
2022-11-29 12:59:51 +0100

    f1, s2: test if CitySearch exists

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,10 +2,22 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from '../App';
 import EventList from '../EventList';
+import CitySearch from '../CitySearch';
 
+// test App.js component
 describe('<App /> component', () => {
+    let AppWrapper;
+    beforeAll(() => {
+        AppWrapper = shallow(<App />);
+    });
+    
+    // Feature 1, Scenario 1
     test('render list of events', () => {
-        const AppWrapper = shallow(<App />);
         expect(AppWrapper.find(EventList)).toHaveLength(1);
+    });
+
+    // Feature 1, Scenario 2
+    test('render CitySearch', () => {
+        expect(AppWrapper.find(CitySearch)).toHaveLength(1);
     });
 });
