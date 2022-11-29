08e889337b69433d062b63e492138cb15fb95858
Amelie <aac.berry@gmail.com>
2022-11-29 20:09:44 +0100

    f3: import NumberOfEvents to app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
 
 function App() {
   return (
     <div className="App">
       <CitySearch />
+      <NumberOfEvents />
       <EventList />
     </div>
   );
