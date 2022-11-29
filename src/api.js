60a200d5c1733bafbc5f43767a7c77f4cbd0bdc7
Amelie <aac.berry@gmail.com>
2022-11-29 12:57:48 +0100

    f1, s2: extracts locations and stores in new array

---
+++ src/api.js
@@ -0,0 +1,10 @@
+/**
+ * This function takes an events array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+ */
+
+export const extractLocations = (events) => {
+    let extractLocations = events.map((event) => event.location);
+    let locations = [...new Set(extractLocations)];
+    return locations;
+};
