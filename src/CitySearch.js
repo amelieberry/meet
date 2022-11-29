77cd585a2d8c42453a1e8f76830c4f34a132e334
Amelie <aac.berry@gmail.com>
2022-11-29 12:56:51 +0100

    f1, s2: show suggestion list on search

---
+++ src/CitySearch.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class CitySearch extends Component {
+    state = {
+        query: '',
+        suggestions: []
+    }
+
+    handleInputChange = (event) => {
+        const value = event.target.value;
+        const suggestions = this.props.locations.filter((location) => {
+            return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+        })
+        this.setState({ 
+            query: value,
+            suggestions, 
+        });
+    }
+
+    render() {
+        return (
+            <div className="CitySearch">
+                <input
+                    type="text"
+                    className="city"
+                    value={this.state.query}
+                    onChange={this.handleInputChange}
+                />
+                <ul className="suggestions">
+                    {this.state.suggestions.map((suggestion) => (
+                        <li key={suggestion}>{suggestion}</li>
+                    ))}
+                    <li key='all'>
+                        <b>See all cities</b>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default CitySearch;
