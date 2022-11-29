36ce6eca593cdc475df065381d568cef553b0bff
Amelie <aac.berry@gmail.com>
2022-11-29 18:21:00 +0100

    component now renders events with info hidden

--- src/Event.js
+++ src/Event.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Event() {
-    return (
-        <div></div>
-    );
+class Event extends Component {
+    state = { isHidden: true };
+
+    render() {
+        const { event } = this.props;
+        const { isHidden } = this.state;
+        return (
+            <div className="event">
+                <h2 className="summary">{event.summary}</h2>
+                <div className="information">
+                    <p className="date">{event.start.dateTime}</p>
+                    <p className="time">{event.start.timeZone}</p>
+                    <p className="location">{event.location}</p>
+                </div>
+                {!isHidden && (
+                    <div className="details">
+                        <p className="details-title">About the event:</p>
+                    </div>
+                )}
+                <button className="toggle-details">{isHidden ? 'Show Details' : 'Hide Details' }</button>
+            </div>     
+        );
+    }
 };
 
 export default Event;
