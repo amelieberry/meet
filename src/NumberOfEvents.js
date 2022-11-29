25d04b0795c83e6922f059a05acf7039b08c5438
Amelie <aac.berry@gmail.com>
2022-11-29 20:09:15 +0100

    f3: created NumberOfEvents component

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class NumberOfEvents extends Component {
+    render() {
+        return (
+            <div className="NumberOfEvents"></div>
+        )
+    }
+}
+
+export default NumberOfEvents;
