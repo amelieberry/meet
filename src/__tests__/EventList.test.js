2e35b626f40d5fc9bdf4073974bcf7ed682dbf71
Amelie <aac.berry@gmail.com>
2022-11-29 13:00:30 +0100

    added comment

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -3,6 +3,7 @@ import { shallow } from "enzyme";
 import EventList from "../EventList";
 import Event from '../Event';
 
+// test EventList and Event components
 describe('<EventList /> component', () => {
     test('render correct number of events', () => {
         const EventListWrapper = shallow(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
