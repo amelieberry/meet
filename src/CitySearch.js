00713f238fed76dbf13a8dac4f34ba6fcc66cc4f
Amelie <aac.berry@gmail.com>
2022-11-29 13:29:44 +0100

    f1, s3: handle clicked city

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -17,6 +17,12 @@ class CitySearch extends Component {
         });
     }
 
+    handleItemClicked = (suggestion) => {
+        this.setState({
+            query: suggestion
+        });
+    }
+
     render() {
         return (
             <div className="CitySearch">
@@ -28,7 +34,10 @@ class CitySearch extends Component {
                 />
                 <ul className="suggestions">
                     {this.state.suggestions.map((suggestion) => (
-                        <li key={suggestion}>{suggestion}</li>
+                        <li 
+                        key={suggestion}
+                        onClick={() => this.handleItemClicked(suggestion)}
+                        >{suggestion}</li>
                     ))}
                     <li key='all'>
                         <b>See all cities</b>
