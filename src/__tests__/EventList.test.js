1c4b0330b7f97c82074a9a477757649e8e4d374b
Amelie <aac.berry@gmail.com>
2022-11-29 13:32:31 +0100

    updated evenlist test to use mockData

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -2,11 +2,12 @@ import React from "react";
 import { shallow } from "enzyme";
 import EventList from "../EventList";
 import Event from '../Event';
+import { mockData } from "../mock-data";
 
 // test EventList and Event components
 describe('<EventList /> component', () => {
     test('render correct number of events', () => {
-        const EventListWrapper = shallow(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
-        expect(EventListWrapper.find(Event)).toHaveLength(4);
+        const EventListWrapper = shallow(<EventList events={mockData} />);
+        expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
     });
 });
