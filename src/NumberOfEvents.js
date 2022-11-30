ee72e9672ffa3571127bc3f2007a3092be4871c8
Amelie <aac.berry@gmail.com>
2022-11-30 12:53:06 +0100

    f3: specify num of events listed in input

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 
 class NumberOfEvents extends Component {
+    state = {
+        eventsNum: '32',
+    };
+
+    handleInputChange = (event) => {
+        const value = event.target.value;
+        this.setState({ 
+            eventsNum: value, 
+        });
+    };
+
     render() {
         return (
-            <div className="NumberOfEvents"></div>
+            <div className="NumberOfEvents">
+                <label>
+                    Number of Events
+                    <input 
+                    className="NumberOfEvents-input"
+                    type="number"
+                    value={this.state.eventsNum}
+                    onChange={this.handleInputChange}
+                    ></input>
+                </label>
+            </div>
         )
     }
 }
