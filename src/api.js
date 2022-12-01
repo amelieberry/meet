560f24e86ac49c793795ef7f186566fdd9ce2e27
Amelie <aac.berry@gmail.com>
2022-12-01 15:21:21 +0100

    added getEvents from mockData

--- src/api.js
+++ src/api.js
@@ -1,8 +1,13 @@
+import { mockData } from "./mock-data";
+
+export const getEvents = async () => {
+    return mockData;
+}
+
 /**
  * This function takes an events array, then uses map to create a new array with only locations.
  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
  */
-
 export const extractLocations = (events) => {
     let extractLocations = events.map((event) => event.location);
     let locations = [...new Set(extractLocations)];
