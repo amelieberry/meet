d6f36321859f2b7184178a8dcf7b37b6e810b925
Amelie <aac.berry@gmail.com>
2022-12-02 17:13:06 +0100

    style events list

--- src/EventList.js
+++ src/EventList.js
@@ -4,13 +4,16 @@ import Event from "./Event";
 function EventList(props) {
     const { events } = props;
     return (
-        <ul className="EventList">
-            {events.map(event =>
-                <li key={event.id}>
-                    <Event event={event} />
-                </li>
+        <div className="w-full md:w-4/5 lg:w-9/12">
+            <h2 className="text-4xl text-center font-bold text-white">Events</h2>
+            <ul className="EventList grid lg:grid-cols-2">
+                {events.map(event =>
+                    <li className="h-min m-8 p-6 bg-white/75 text-navy rounded-xl" key={event.id}>
+                        <Event event={event} />
+                    </li>
                 )}
-        </ul>
+            </ul>
+        </div>
     ); 
 };  
 
