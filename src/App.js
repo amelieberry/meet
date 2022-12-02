a3642a1e610d7115a9cb834a15f4811b67885e63
Amelie <aac.berry@gmail.com>
2022-12-02 18:57:48 +0100

    integrated final API

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { extractLocations, getEvents } from './api';
+import './nprogress.css';
 
 class App extends Component {
   state = {
--- src/api.js
+++ src/api.js
@@ -1,7 +1,81 @@
+import axios from "axios";
 import { mockData } from "./mock-data";
+import NProgress from 'nprogress';
+
+export const getAccessToken = async () => {
+    const accessToken = localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || tokenCheck.error) {
+        await localStorage.removeItem('access_token');
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = await searchParams.get('code');
+        if (!code) {
+            const results = await axios.get('https://zcxuszj2qi.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url');
+            const { authUrl } = results.data;
+            return (window.location.href = authUrl);
+        }
+        return code && getToken(code);
+    }
+    return accessToken
+}
+
+// takes the accessToken you found and checks whether it’s a valid token or not
+const checkToken = async (accessToken) => {
+    const result = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`)
+    .then((res) => res.json())
+    .catch((error) => error.json());
+    return result;
+};
 
 export const getEvents = async () => {
-    return mockData;
+    NProgress.start();
+
+    if (window.location.href.startsWith('http://localhost')) {
+        NProgress.done();
+        return mockData;
+    }
+
+    const token = await getAccessToken();
+
+    if (token) {
+        removeQuery();
+        const url = 'https://zcxuszj2qi.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
+        const result = await axios.get(url);
+        if (result.data) {
+            let locations = extractLocations(result.data.events);
+            localStorage.setItem('lastEvents', JSON.stringify(result.data));
+            localStorage.setItem('locations', JSON.stringify(locations));
+        }
+        NProgress.done();
+        return result.data.events;
+    } 
+}
+
+//  check whether there’s a path, then build the URL with the current path (or build the URL without a path using window.history.pushState()).
+const removeQuery = () => {
+    if (window.history.pushState && window.location.pathname) {
+        let newurl = 
+            window.location.protocol +
+            '//' +
+            window.location.host +
+            window.location.pathname;
+        window.history.pushState('', '', newurl);
+    }
+};
+
+const getToken = async (code) => {
+    const encodeCode = encodeURIComponent(code);
+    const { access_token } = await fetch(
+        'https://zcxuszj2qi.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+    )
+    .then((res) => {
+        return res.json();
+    })
+    .catch((error) => error);
+
+    access_token && localStorage.setItem('access_token', access_token);
+    return access_token;
 }
 
 /**
