df6958a5a3a58889e9c0ec3e06f5f03491793d9d
Amelie <aac.berry@gmail.com>
2022-12-07 18:40:05 +0100

    BDD: test feature 3, scenarios 1 and 2, pass

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -2,12 +2,13 @@ import React from "react";
 import { mount } from "enzyme";
 import { loadFeature, defineFeature } from "jest-cucumber";
 import App from "../App";
+import NumberOfEvents from "../NumberOfEvents";
 
 const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
 
 // FEATURE 3
 defineFeature(feature, test => {
-
+    let AppWrapper;
     // Scenario 1
     test('When user has not specified a number, 32 is the default', ({ given, when, then }) => {
         given('the number of shown event was not specified', () => {
@@ -15,26 +16,29 @@ defineFeature(feature, test => {
         });
 
         when('the event list is displayed', () => {
-
+            AppWrapper = mount(<App />);
         });
 
         then('the user should see a list of 32 events per page by default.', () => {
-
+            AppWrapper.update();
+            expect(AppWrapper.state('eventsNumber')).toEqual(32);
         });
     });
 
     // Scenario 2
     test('User can expand an event to see its details', ({ given, when, then }) => {
-        given('the number of shown event was specified', () => {
-
+        given('the number of shown event was specified', async () => {
+            AppWrapper = await mount(<App />);
         });
 
-        when('the the event list is displayed', () => {
-
+        when('the event list is displayed', () => {
+            AppWrapper.update();
+            let NumberOfEventsWrapper = AppWrapper.find(NumberOfEvents);
+            NumberOfEventsWrapper.find('.NumberOfEvents-input').simulate('change', { target: { value: 1 } });
         });
 
         then('user should see a list of the specified amount of events per page.', () => {
-
+            expect(AppWrapper.find('.EventList')).toHaveLength(1);
         });
     });
 })
