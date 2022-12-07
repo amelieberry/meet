caf40c3bd1f05c57fd4609da9f6c71cc9294f465
Amelie <aac.berry@gmail.com>
2022-12-07 15:56:57 +0100

    E2E: test feature 2 scenario 2, pass

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -2,17 +2,33 @@ import puppeteer from "puppeteer"
 
 // Feature 2 - Show / hide an event's details
 describe('show/hide an event details', () => {
-    // Scenario 1
-    test('An event element is collapsed by default', async () => {
-        const browser = await puppeteer.launch(); 
-        const page = await browser.newPage();
+    let browser, page;
+    beforeAll(async () => {
+        jest.setTimeout(30000);
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
         await page.goto('http://localhost:3000/');
         await page.waitForSelector('.event');
-        const eventDetails = await page.$('.event .details');
-        expect(eventDetails).toBeNull();
+    });
+
+    afterAll(() => {
         browser.close();
     });
-    // Scenario 2 - User can expand an event to see its details
+
+    // Scenario 1
+    test('An event element is collapsed by default', async () => {
+        const eventDetails = await page.$('.event .details');
+        expect(eventDetails).toBeNull();
+    });
+
+    // Scenario 2
+    test('User can expand an event to see its details', async () => {
+        await page.click('.event .toggle-details');
+        const eventDetails = await page.$('.event .details');
+        expect(eventDetails).toBeDefined();
+        
+    })
+
     // Scenario 3 - User can collapse an event to hide its details
     
 })
