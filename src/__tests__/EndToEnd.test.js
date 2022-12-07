8bb5e2323dfc524540428be84e7d8a8752b328e6
Amelie <aac.berry@gmail.com>
2022-12-07 16:05:56 +0100

    E2E: test feature 2 scenario 3, pass

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,11 +1,15 @@
 import puppeteer from "puppeteer"
 
-// Feature 2 - Show / hide an event's details
+// FEATURE 2
 describe('show/hide an event details', () => {
     let browser, page;
     beforeAll(async () => {
         jest.setTimeout(30000);
-        browser = await puppeteer.launch();
+        browser = await puppeteer.launch({
+            // headless: false,
+            // slowMo: 250,
+            // ignoreDefaultArgs: ['--disable-extensions'] 
+        });
         page = await browser.newPage();
         await page.goto('http://localhost:3000/');
         await page.waitForSelector('.event');
@@ -26,9 +30,12 @@ describe('show/hide an event details', () => {
         await page.click('.event .toggle-details');
         const eventDetails = await page.$('.event .details');
         expect(eventDetails).toBeDefined();
-        
     })
 
-    // Scenario 3 - User can collapse an event to hide its details
-    
+    // Scenario 3
+    test('User can collapse an event to hide its details', async () => {
+        await page.click('.event .toggle-details');
+        const eventDetails = await page.$('.event .details');
+        expect(eventDetails).toBeNull();
+    })
 })
