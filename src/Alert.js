ec106c22af42b3b0fb29ec9cc99bc238fa8ce1e4
Amelie <aac.berry@gmail.com>
2022-12-13 11:58:44 +0100

    Added ErrorAlert class

--- src/Alert.js
+++ src/Alert.js
@@ -28,5 +28,12 @@ class InfoAlert extends Alert {
     }
 }
 
-export { InfoAlert };
+class ErrorAlert extends Alert {
+    constructor(props) {
+        super(props);
+        this.color = 'red';
+    }
+}
+
+export { InfoAlert, ErrorAlert };
 
