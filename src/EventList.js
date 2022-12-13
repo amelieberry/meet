5104ee78c9252bf6cfee6d2fc11b76d84e8cea8a
Amelie <aac.berry@gmail.com>
2022-12-13 19:01:43 +0100

    event list displays correct number by default

--- src/EventList.js
+++ src/EventList.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Event from "./Event";
+// import NumberOfEvents from "./NumberOfEvents";
 
 function EventList(props) {
-    const { events } = props;
+    const { events, eventsNumber } = props;
     return (
         <div className="w-full md:w-4/5 lg:w-9/12">
             <h2 className="text-4xl text-center font-bold text-white">Events</h2>
             <ul className="EventList grid lg:grid-cols-2">
-                {events.map(event =>
+                {events.slice(eventsNumber ? -eventsNumber : events.length).map(event =>
                     <li className="h-min m-8 p-6 bg-white/75 text-navy rounded-xl" key={event.id}>
                         <Event event={event} />
                     </li>
