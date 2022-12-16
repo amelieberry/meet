17f645f47b0ab19271789f52931ed1d1e0ee96ec
Amelie <aac.berry@gmail.com>
2022-12-16 09:33:47 +0100

    load eventList from localStorage if offline

--- src/api.js
+++ src/api.js
@@ -36,6 +36,12 @@ export const getEvents = async () => {
         return mockData;
     }
 
+    if (!navigator.onLine) {
+        const data = localStorage.getItem('lastEvents');
+        NProgress.done();
+        return data?JSON.parse(events).events:[];;
+    }
+
     const token = await getAccessToken();
 
     if (token) {
