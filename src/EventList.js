dc3d5257756aadeb02dcb8ea43c2ef7564a47ec9
Amelie <aac.berry@gmail.com>
2022-12-16 17:33:56 +0100

    updated colors of event cards

--- src/EventList.js
+++ src/EventList.js
@@ -6,10 +6,10 @@ function EventList(props) {
     const { events, eventsNumber } = props;
     return (
         <div className="w-full md:w-4/5 lg:w-9/12">
-            <h2 className="text-4xl text-center font-bold text-coral">Events</h2>
+            <h2 className="text-4xl text-center font-bold text-navy dark:text-coral">Events</h2>
             <ul className="EventList grid lg:grid-cols-2">
                 {events.slice(eventsNumber ? -eventsNumber : events.length).map(event =>
-                    <li className="h-min m-8 p-6 bg-navy/75 text-white dark:bg-white/75 dark:text-navy rounded-xl" key={event.id}>
+                    <li className="h-min m-8 p-6  bg-white/75 text-navy rounded-xl" key={event.id}>
                         <Event event={event} />
                     </li>
                 )}
