cc80267023eb01674e548ddffa3e36e45ed1c073
Amelie <aac.berry@gmail.com>
2022-12-19 14:31:38 +0100

    export checkToken (import in app.js)

--- src/api.js
+++ src/api.js
@@ -21,7 +21,7 @@ export const getAccessToken = async () => {
 }
 
 // takes the accessToken you found and checks whether it’s a valid token or not
-const checkToken = async (accessToken) => {
+export const checkToken = async (accessToken) => {
     const result = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`)
     .then((res) => res.json())
     .catch((error) => error.json());
