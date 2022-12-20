7c02b4d3bdec44c6828402d0445fd539efef65b6
Amelie <aac.berry@gmail.com>
2022-12-20 10:48:58 +0100

    fixed margin messing up layout on sm screen

--- src/EventList.js
+++ src/EventList.js
@@ -6,10 +6,10 @@ function EventList(props) {
     const { events, eventsNumber } = props;
     return (
         <div className="w-full md:w-4/5 lg:w-9/12">
-            <h2 className="text-4xl text-center font-bold text-navy dark:text-coral">Events</h2>
+            <h2 className="text-4xl mb-4 text-center font-bold text-navy dark:text-coral">Events</h2>
             <ul className="EventList grid lg:grid-cols-2 ">
                 {events.slice(eventsNumber ? -eventsNumber : events.length).map(event =>
-                    <li className="h-min m-8 p-6 bg-navy/10 dark:bg-dark-navy/50 text-navy dark:text-white rounded-xl" key={event.id}>
+                    <li className="h-min m-4 p-6 bg-navy/10 dark:bg-dark-navy/50 text-navy dark:text-white rounded-xl" key={event.id}>
                         <Event event={event} />
                     </li>
                 )}
