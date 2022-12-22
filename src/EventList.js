122de45814b5509deaae0d4feca79be44a7b69ee
Amelie <aac.berry@gmail.com>
2022-12-22 15:33:49 +0100

    added date and time sorting to resized eventlist

--- src/EventList.js
+++ src/EventList.js
@@ -8,7 +8,7 @@ function EventList(props) {
         <div className="w-full md:w-4/5 lg:w-9/12">
             <h2 className="text-4xl mb-4 text-center font-bold text-navy dark:text-coral mt-4">Events</h2>
             <ul className="EventList grid lg:grid-cols-2 ">
-                {events.slice(eventsNumber ? -eventsNumber : events.length).map(event =>
+                {(eventsNumber ? events.slice(0, eventsNumber) : events.slice(events.length)).map(event =>
                     <li className="h-min m-4 p-6 bg-navy/10 dark:bg-navy text-navy dark:text-white rounded-xl border-solid border-2 border-coral" key={event.id}>
                         <Event event={event} />
                     </li>
