b00c967193eb849002a2403b40b5d0b7bc8536d8
Amelie <aac.berry@gmail.com>
2022-12-22 11:08:37 +0100

    added handleFocus to show suggestions + restyle

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -9,16 +9,30 @@ class CitySearch extends Component {
         infoText: ''
     }
 
+    handleFocus = (event) => {
+        const value = event.target.value;
+        this.setState({
+            showSuggestions: true
+        })
+        if (!value || value === 'all') {
+            this.setState({
+                suggestions: this.props.locations
+            })
+        } else {
+            this.handleInputChange(event)
+        }
+    }
+
     handleInputChange = (event) => {
         const value = event.target.value;
-        this.setState({showSuggestions:true});
+        this.setState({ showSuggestions: true });
         const suggestions = this.props.locations.filter((location) => {
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         });
         if (suggestions.length === 0) {
             this.setState({
                 query: value,
-                infoText: 'City not found, please try another city',
+                infoText: 'City not found, try again',
             });
         } else {
             return this.setState({
@@ -42,25 +56,40 @@ class CitySearch extends Component {
 
     render() {
         return (
-            <div className="CitySearch w-full md:w-3/6 flex items-center flex-col">
-                <label className="block text-lg font-semibold mb-2" >Select a city:
-                <input
-                    type="text"
-                    className="city block p-2 rounded-md text-base font-normal w-72 mt-2 text-dark-navy bg-white/80 dark:bg-light-blue/80"
-                    value={this.state.query}
-                    onChange={this.handleInputChange}
-                    onFocus={() => { this.setState({ showSuggestions: true }) }}
-                />
+            <div className="CitySearch flex items-center flex-col m-2">
+                <label className="block text-lg font-semibold" >Select a City
+                    <input
+                        id="city-input"
+                        type="text"
+                        className="city block p-2 rounded-md text-base font-normal w-72 text-dark-navy bg-white dark:bg-light-blue focus:outline-none"
+                        value={this.state.query}
+                        onChange={this.handleInputChange}
+                        onFocus={this.handleFocus}
+                    />
                 </label>
-                <InfoAlert className="dark:text-white" text={this.state.infoText} />
-                <ul className="suggestions w-72 rounded-md mt-2 text-center" style={this.state.showSuggestions ? {} : { display: 'none' }}>
+                <ul 
+                    className="suggestions h-36 max-h-40 overflow-auto mt-2 w-72 rounded-md text-center bg-white dark:bg-light-blue dark:text-navy z-90" 
+                    style={this.state.showSuggestions ? {} : { display: 'none' }}
+                >
+                    <div className="block text-right h-7">
+                        <button 
+                            className="bg-light-blue dark:bg-white h-6 w-6 rounded-full mt-1 mr-1"
+                            onClick={() => { this.setState({ showSuggestions: false })} }
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z" clipRule="evenodd" />
+                            </svg>
+
+                        </button>
+                    </div>
+                    <InfoAlert className="text-normal" text={this.state.infoText} />
                     {this.state.suggestions.map((suggestion) => (
                         <li
                             className="cursor-pointer"
                             key={suggestion}
                             onClick={() => this.handleItemClicked(suggestion)}
                         >{suggestion}</li>
-                    ))}
+                    ))} {console.log(this.state.suggestions)}
                     <li onClick={() => this.handleItemClicked("all")}>
                         <b className="cursor-pointer">See all cities</b>
                     </li>
